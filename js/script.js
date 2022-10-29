82ad9c6cfbe6e5e82afc1d63e6673c4c3ba7c3d3
Anton <shadoff1996@yandex.ru>
2022-10-29 21:05:11 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+let incr = 10,
+    decro = 10;
+
+++incr;
+--decro;
+
+console.log(++incr);
+console.log(--decro);
+
+console.log(5 % 2);
+
+console.log(2 * 4 !== 8);
+
+const isTrue = true,
+      isChecked = false;
+
+console.log(isTrue && !isChecked);
+
+console.log(!isChecked || isTrue);
