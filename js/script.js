29a9c34e54cceb5feb6a35b0b867f88d9becfcaa
Anton <shadoff1996@yandex.ru>
2022-10-30 09:11:53 +0300

    I've done the third task

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 'use strict';
 
-const numberOfFilms = prompt('How many films do you watch?', '');
+let numberOfFilms = prompt('How many films do you watch?', '');
+
+if (numberOfFilms === null) {
+    numberOfFilms = prompt('How many films do you watch?', '');
+}
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,12 +14,28 @@ const personalMovieDB = {
     privat: false,
 };
 
-const movies = {};
-
 for (let i = 0; i < 2; i++) {
     let lastFilm = prompt('What is your last watched film?', '');
+    console.log(lastFilm);    
+    while (lastFilm == '' || lastFilm == null) {
+        lastFilm = prompt('What is your last watched film?', '');
+    }
+
+    if (lastFilm.length > 50) {
+        lastFilm = prompt('What is your last watched film?', '');
+    }
+
     let ratingLastFilm = prompt('What is the rating of the last film?', '');
-    movies[lastFilm] = ratingLastFilm;
+
+    personalMovieDB.movies[lastFilm] = ratingLastFilm;
 }
 
-console.log(movies, personalMovieDB);
+if (personalMovieDB.count < 10) {
+    console.log('Too few movies have been viewed');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("You're a classical watcher");
+} else if (personalMovieDB > 30) {
+    console.log(`You're a movie fan`);
+} else {
+    console.log('An error has occurred');
+}
